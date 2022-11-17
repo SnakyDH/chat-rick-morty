ef845254aa819f0ce411b6e374b6f2f9c55f95db
Daniel García Romero <skdanielg5@gmail.com>
2022-11-16 23:02:41 -0500

    [Update] Config Files and gitignore

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const config = {
   server: {
-    port: process.env.port || 3000,
+    port: process.env.PORT || 3000,
   },
   db: {
-    host: process.env.host || 'localhost',
-    user: process.env.user || 'root',
-    password: process.env.password || '',
-    port: process.env.db_port || 3306,
-    name: process.env.db_name || 'chat-rick-morty',
+    host: process.env.HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    port: process.env.DB_PORT || 3306,
+    name: process.env.DB_NAME || 'chat-rick-morty',
   },
 };
