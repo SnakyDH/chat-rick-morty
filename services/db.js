2c4b7685bd88b634991076a4b4b6186271a76005
Daniel García Romero <skdanielg5@gmail.com>
2022-11-16 23:04:01 -0500

    [ADD] add conection with DB and socket.io server

--- services/db.js
+++ services/db.js
@@ -1,4 +1,5 @@
-import config from '../config/config.js';
+import { config } from '../config/config.js';
+
 import { createPool } from 'mariadb';
 
 export const pool = createPool({
---
+++ services/realtimeServer.js
@@ -0,0 +1,7 @@
+import { Server } from 'socket.io';
+export const realtimeServer = (httpServer) => {
+  const io = new Server(httpServer);
+  io.on('connection', (socket) => {
+    console.log(socket.id);
+  });
+};
