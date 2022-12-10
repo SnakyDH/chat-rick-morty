b1384b0c3823371402c5d88e710e51af7d7e1c03
Daniel García Romero <skdanielg5@gmail.com>
2022-12-10 14:17:29 -0500

    [DELETE] api chat files

--- api/chat/controller.js
+++
@@ -1,6 +0,0 @@
-export const verifyCookie = async (req, res) => {
-  const cookie = req.cookie.email;
-  //const user = cookie.split('=').pop();
-
-  res.json(cookie);
-};
--- api/chat/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { verifyCookie } from './controller.js';
-const router = Router();
-
-// localhost:3000/
-
-router.post('/chat', verifyCookie);
-
-export default router;
