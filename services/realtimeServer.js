9ddb62af9c97d9c3416f031fc93aa0a4f18925a6
Daniel García Romero <skdanielg5@gmail.com>
2022-12-12 02:04:44 -0500

    [COMPLETE] v0.1 chat funtionality

--- services/realtimeServer.js
+++ services/realtimeServer.js
@@ -1,7 +1,21 @@
 import { Server } from 'socket.io';
 export const realtimeServer = (httpServer) => {
   const io = new Server(httpServer);
-  io.on('connection', (socket) => {
+  const rickTeam = io.of('rick');
+  const mortyTeam = io.of('morty');
+  rickTeam.on('connection', (socket) => {
     console.log(socket.id);
+    socket.on('send-message', ({ message, users }) => {
+      rickTeam.emit('messages', message, users);
+      console.log('Message to rick', message);
+      console.log('user aparte', users);
+    });
+  });
+  mortyTeam.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('send-message', ({ message, users }) => {
+      mortyTeam.emit('messages', message, users);
+      console.log('Message to morty', message);
+    });
   });
 };
