a6ef466bbf7e5862a30d97e6d48a5de718ef891e
Daniel García Romero <skdanielg5@gmail.com>
2022-12-12 02:02:49 -0500

    [DELETE] unnecessary script

--- scripts/scrapper.rick-morty.js
+++ scripts/scrapper.rick-morty.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/* import axios from 'axios';
 
 export function getData(character) {
   axios
@@ -17,3 +17,4 @@ export function getData(character) {
       console.error(error);
     });
 }
+ */
